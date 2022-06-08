95f6a8e4fae83f98323d45c022528cf476d16e2f
Kimhyunwoo72 <ekfvoddl0321@gmail.com>
2022-06-08 23:26:09 +0900

    0608

--- src/App.js
+++ src/App.js
@@ -3,70 +3,103 @@ import "./App.scss";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
-// import { useEffect, useState } from "react";
 
+const Width = window.innerWidth;
+console.log(Width);
 export default function App() {
-  // const [position, setPosition] = useState(0);
-
-  // function onScroll() {
-  //   console.log(window.scrollY);
-  //   setPosition(window.scrollY);
-  // }
-  // useEffect(() => {
-  //   window.addEventListener("scroll", onScroll);
-  //   return () => {
-  //     window.addEventListener("scroll", onScroll);
-  //   };
-  // }, []);
   useEffect(() => {
-    // let SECTIONS = gsap.utils.toArray(".imgWrap");
+    if (Width >= 600) {
+      gsap.to(".img1", {
+        scrollTrigger: {
+          trigger: ".img1",
+          start: "top",
+          markers: true,
+          end: "50%",
+          scrub: 1,
+        },
+        duration: 2,
+        scale: 0.5,
+      });
+      gsap.to(".img2", {
+        scrollTrigger: {
+          trigger: ".img1",
+          start: "top",
+          markers: true,
+          scrub: 1,
+          end: "50%",
+        },
+        duration: 2,
+        scale: 1,
+      });
+      gsap.to(".imgg", {
+        scrollTrigger: {
+          trigger: ".img2",
+          start: "top",
+          markers: true,
+          scrub: 1,
+          end: "50%",
+        },
+        duration: 2,
+        scale: 0.5,
+      });
 
-    gsap.to(".img1", {
-      scrollTrigger: {
-        trigger: ".img1",
-        start: "top",
-        markers: true,
-        end: "50%",
-        scrub: 1,
-      },
-      duration: 2,
-      scale: 0.5,
-    });
-    gsap.to(".img2", {
-      scrollTrigger: {
-        trigger: ".img1",
-        start: "top",
-        markers: true,
-        scrub: 1,
-        end: "50%",
-      },
-      duration: 2,
-      scale: 1,
-    });
-    gsap.to(".imgg", {
-      scrollTrigger: {
-        trigger: ".img2",
-        start: "top",
-        markers: true,
-        scrub: 1,
-        end: "50%",
-      },
-      duration: 2,
-      scale: 0.5,
-    });
+      gsap.to(".img3", {
+        scrollTrigger: {
+          trigger: ".img2",
+          start: "top",
+          markers: true,
+          scrub: 1,
+          end: "70%",
+        },
+        duration: 2,
+        scale: 1,
+      });
+    } else {
+      gsap.to(".img1", {
+        scrollTrigger: {
+          trigger: ".img1",
+          start: "top",
+          markers: true,
+          scrub: 1,
+        },
+        duration: 2,
+        scale: 0.5,
+      });
+      gsap.to(".img2", {
+        scrollTrigger: {
+          trigger: ".img1",
+          start: "top",
+          markers: true,
+          scrub: 1,
+        },
+        duration: 2,
+        scale: 1,
+      });
+      gsap.to(".imgg", {
+        scrollTrigger: {
+          trigger: ".img2",
+          start: "top",
+          markers: true,
+          scrub: 1,
+        },
+        duration: 2,
+        scale: 0.5,
+      });
 
-    gsap.to(".img3", {
-      scrollTrigger: {
-        trigger: ".img2",
-        start: "top",
-        markers: true,
-        scrub: 1,
-        end: "70%",
-      },
-      duration: 2,
-      scale: 1,
-    });
+      gsap.to(".img3", {
+        scrollTrigger: {
+          trigger: ".img2",
+          start: "top",
+          markers: true,
+          scrub: 1,
+          end: "70%",
+        },
+        duration: 2,
+        scale: 1,
+      });
+    }
   });
+
   return (
     <main className="App">
       <div className="wrap">
